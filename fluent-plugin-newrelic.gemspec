f2ad74e2b0d5fde0d24ff221c09b8d9f42e43e3c
Chandrashekar Tippur <ctippur@gmail.com>
2015-03-04 11:35:44 -0800

    Adding ironio related plugin code

---
+++ fluent-plugin-newrelic.gemspec
@@ -0,0 +1,26 @@
+# -*- encoding: utf-8 -*-
+$:.push File.expand_path("../lib", __FILE__)
+
+Gem::Specification.new do |gem|
+  gem.name          = "fluent-plugin-ironio"
+  gem.version       = "0.0.1"
+  gem.date          = '2015-01-30'
+  gem.authors       = ["chandrashekar Tippur"]
+  gem.email         = ["ctippur@gmail.com"]
+  gem.summary       = %q{Fluentd input plugin for ironio alerts}
+  gem.description   = %q{FLuentd plugin for ironio alerts... WIP}
+  gem.homepage      = 'https://github.com/Bigel0w/fluent-plugin-ironio'
+  gem.license       = 'MIT'
+
+  gem.files         = `git ls-files`.split($\)
+  gem.test_files    = gem.files.grep(%r{^(test|spec|features)/})
+  gem.executables   = gem.files.grep(%r{^bin/}).map{ |f| File.basename(f) }
+  gem.require_paths = ["lib"]
+
+  gem.add_development_dependency "rake", '~> 0.9', '>= 0.9.6'
+  gem.add_runtime_dependency "fluentd", '~> 0.10', '>= 0.10.51'
+  gem.add_runtime_dependency "iron_mq", '>= 5.0.1'
+  gem.add_runtime_dependency "htmlentities", '>= 4.3.3'
+  gem.add_runtime_dependency "daemons", '>= 0'
+  gem.add_runtime_dependency "cgi", '>= 0'
+end
---
+++ lib/fluent/plugin/in_newrelic.rb
@@ -0,0 +1,127 @@
+module Fluent
+
+# Read  trap messages as events in to fluentd
+  class NewRelicInput < Input
+    Fluent::Plugin.register_input('newrelic', self)
+
+    # Define default configurations
+    config_param :tag, :string, :default => "alert.newrelic"
+    config_param :endpoint, :string, :default => "" # Optional
+    config_param :projectId, :string, :default => ""
+    config_param :token, :string, :default => ""
+    config_param :endpointType, :string, :default => "ironio"
+    config_param :oauthId, :string, :default => ""
+    config_param :endpointQueue, :string, :default => ""
+    config_param :interval, :string, :default => "5"
+    config_param :readOnly, :string, :default => "true"
+
+    
+
+    # Initialize and bring in dependencies
+    def initialize
+      super
+      require 'json'
+      require 'daemons'
+      require 'iron_mq'
+      require 'pp'
+    end # def initialize
+
+    # Load internal and external configs
+    def configure(conf)
+      super
+      @conf = conf
+      # TO DO Add code to choke if config parameters are not there
+    end # def configure
+    
+    def start
+      super
+      @loop = Coolio::Loop.new
+      timer_trigger = TimerWatcher.new(@interval, true, &method(:input))
+      timer_trigger.attach(@loop)
+      @thread = Thread.new(&method(:run))
+      $log.info "starting newrelic poller, interval #{@interval}"
+    end
+
+    # Stop Listener and cleanup any open connections.
+    def shutdown
+      super
+      @loop.stop
+      @thread.join
+    end
+
+    def run
+      @loop.run
+      $log.info "Running NewRelic Input"
+    end
+
+    # Start SNMP Trap listener
+    def input
+        @ironmq = IronMQ::Client.new(token: @token, project_id: @projectId) # do |event|
+        # Get a Queue object
+        @queue = @ironmq.queue(@endpointQueue)
+  
+        # Put a message on the queue
+	#msg = @queue.post("hello world!")
+
+	# Get a message
+	msg = @queue.get()
+
+	# Convert to a proper json
+	#rawMsg=CGI::unescape(msg.raw.to_json).gsub(/"alert=/,'').gsub(/}","timeout/, '},"timeout')
+	#rawMsg=CGI::unescape(jsonhtmlencodedraw).gsub(/"alert=/,'').gsub(/}","timeout/, '},"timeout')
+
+	# Convert to hash
+	#jsonhtmlencodedraw=JSON.parse(rawMsg, :quirks_mode => false) # => "50's & 60's"
+	jsonhtmlencodedraw=JSON.parse(URI.decode((msg.body.split("="))[1]))
+	#argosHash=Hash.new {}
+	#argosHash['newraw']=jsonhtmlencodedraw
+	# Add raw json
+	jsonhtmlencodedraw.merge!('newraw'=>URI.decode((msg.body.split("="))[1]))
+
+	#jsonhtmlencodedraw.store('newraw', jsonhtmlencodedraw)
+	#pp argosHash
+
+	# Add evet_type, intermediary_source, received_time
+
+	#tag = @tag 
+	timestamp = Engine.now # Should be received_time_input
+
+	jsonhtmlencodedraw['received_time_input']=timestamp.to_s
+	jsonhtmlencodedraw['event_type']=@tag.to_s
+	jsonhtmlencodedraw['intermediary_source']=jsonhtmlencodedraw['alert_url']
+	#jsonhtmlencodedraw.each do |key, value|
+	#	jsonhtmlencodedraw[:key]='"' + value + '"'
+	#end
+
+	$log.info "Incoming message  #{jsonhtmlencodedraw}"
+	# Try catch
+	#record = {"value"=> jsonhtmlencodedraw,  argosHash.to_json,"tags"=>{"type"=>"alert","application_name"=>jsonhtmlencodedraw["body"]["application_name"]}}
+	begin
+		#Engine.emit('"' + @tag.to_s + '"', '"' + timestamp.to_s + '"' , jsonrawstr)
+		Engine.emit( @tag.to_s ,  timestamp.to_i , jsonhtmlencodedraw)
+	rescue Exception => e
+		puts e.message 
+		puts e.backtrace.inspect
+	end
+
+	# Delete the message. Make this configurable
+	if @readOnly == "false" 
+	  res = msg.delete # or @queue.delete(msg.id)
+        end
+    end # def Input
+
+  end # End Input class
+
+  class TimerWatcher < Coolio::TimerWatcher
+	def initialize(interval, repeat, &callback)
+	  @callback = callback
+	  super(interval, repeat)
+	end
+
+	def on_timer
+	  @callback.call
+	end
+  end
+
+end # module Fluent
+
